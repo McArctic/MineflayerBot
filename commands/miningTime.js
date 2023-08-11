ca96d25d50eb07019740fedff3f1ccf69921180e
McArctic <creepergaming490@gmail.com>
2023-08-11 00:20:17 -0400

    Made Some Changes. Mining Command Broke tho

--- commands/miningTime.js
+++ commands/miningTime.js
@@ -1,79 +1,14 @@
-const { Movements, goals } = require('mineflayer-pathfinder');
+const { pathfinder, Movements, goals} = require('mineflayer-pathfinder');
+const mineflayer = require('mineflayer');
 
-const toolTypes = {
-  diamond_ore: 'diamond_pickaxe',
-  // Add other block types and corresponding tool types here
-};
-
-let currentTool = null;  // Define currentTool here
-
-async function getBestToolForBlock(bot, blockType) {
-  const bestToolType = toolTypes[blockType];
-  if (!bestToolType) return null;
-
-  if (currentTool?.name === bestToolType) return currentTool;
-
-  const bestTool = bot.inventory.items().find(item => item.name === bestToolType);
-  if (!bestTool) return null;
-
-  currentTool = bestTool;
-  await equipBestTool(bot, blockType);
-
-  return bestTool;
-}
-
-async function equipBestTool(bot, blockType) {
-  const tool = bot.pathfinder.bestHarvestTool({ name: blockType });
-  if (tool) {
-    try {
-      await bot.equip(tool, 'hand');
-    } catch (error) {
-      console.error('Failed to equip tool:', tool, '\n', error);
-    }
-  }
-}
-
-function restartPathfinding(bot) {
-  bot.pathfinder.setGoal(null);
-  setTimeout(() => {
-    const currentGoal = bot.pathfinder.goal;
-    if (currentGoal) {
-      bot.pathfinder.setGoal(currentGoal);
-    }
-  }, 10000); // Adjust the delay as needed
-}
-
-function startMiningTunnel(bot, targetY, tunnelDistance) {
-  console.log("Starting mining tunnel...");
-
-  const bestTool = getBestToolForBlock(bot, 'diamond_ore');
-  if (!bestTool) {
-    console.log("No suitable tool found.");
-    return;
-  }
-
-  const movement = new Movements(bot, bestTool.equipType);
-  bot.pathfinder.setMovements(movement);
-
-  const goal = new goals.GoalBlock(tunnelDistance, targetY, 0.5);
-  console.log("Setting goal:", goal);
-  bot.pathfinder.setGoal(goal);
-
-  bot.on('path_update', (results) => {
-    if (!results.path.length) {
-      console.log("No path found.");
-      restartPathfinding(bot);
-    }
-  });
-
-  bot.on('stuck', () => {
-    console.log("Bot is stuck. Restarting pathfinding...");
-    restartPathfinding(bot);
-  });
-}
+//NOT WORKING
+// GETS STUCK RANDOMLY
+// 0 FUCKING CLUE WHY
+// THINKING OF FIX OF LIKE IF THE SAME CORDS ARE REPEATED WITH NO CHANGE THEN TRY AND BREAK BLOCKS AROUND IT TILL IT CAN MOVE AGAIN AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 
 
 module.exports = function(bot) {
+
   bot.on('chat', (username, message) => {
     if (username === bot.username) return;
 
@@ -85,7 +20,7 @@ module.exports = function(bot) {
       const tunnelDistance = parseFloat(match[2]);
       
       if (!isNaN(targetY) && !isNaN(tunnelDistance)) {
-        startMiningTunnel(bot, targetY, tunnelDistance);
+        goToYLevel(bot, targetY, tunnelDistance);
         bot.chat(`Heading to Y ${targetY} and starting a ${tunnelDistance} block tunnel.`);
         return;
       }
@@ -93,4 +28,27 @@ module.exports = function(bot) {
       bot.chat("Usage: !minetime [y value] [Tunnel Distance]");
     }
   });
+
+  async function goToYLevel(bot, targetY, tunnelDistance) {
+    bot.chat("Going To")
+    bot.pathfinder.setGoal(new goals.GoalY(targetY))
+
+    const positionChecker = setInterval(() => {
+      const currentPos = bot.entity.position;
+      if (currentPos) {
+        console.log(`Current Position: X=${currentPos.x}, Y=${currentPos.y}, Z=${currentPos.z}`);
+      }
+    }, 20)
+
+
+
+
+    bot.on('goal_reached', (goal) => {
+      if (goal instanceof goals.GoalY && goal.y === targetY) {
+        clearInterval(positionUpdateInterval);
+        bot.chat('Reached Y Level ' + targetY);
+      }
+    })
+  }
+  
 };
